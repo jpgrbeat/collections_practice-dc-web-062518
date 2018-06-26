4c5bba87026783d7979d776b49ef39522ab9cbf4
Joshua R Miller <joshua.ross.miller@gmail.com>
2018-06-26 01:00:05 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,3 +1,4 @@
+require 'pry'
 def sort_array_asc(arr)
   result = arr.sort
 end
@@ -5,4 +6,8 @@ end
 def sort_array_desc(arr)
   result = arr.sort
   result.reverse
+end
+
+def sort_array_char_count(arr)
+  arr.map{|word| word.length binding.pry}
 end
